d386849e9238f7e0d976cf142babd929af58f45d
erickrona <49962019+erickrona@users.noreply.github.com>
2019-12-06 12:57:37 -0600

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -1,18 +1,105 @@
 
-const express = require('express')
-require('./db/mongoose')
+
+
+
+const express = require('express');
+const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("./db/mongoose");
+
+//const mongoose= require("mongoose");
+
+const TodoTask = require("./models/toDoTask");
+//const port = process.env.PORT || 3000
+
 var cors = require('cors');
 
 const router = require('./routes.js')
 
-const app = express()
-const port = process.env.PORT || 3000
+dotenv.config();
 
 app.use(cors())
 app.use(express.json()) // parsea a json
 app.use(router)
 
+app.use("/static", express.static("public"));
 
-app.listen(port, function() {
-  console.log('Server up and running on port ' + port)
+app.use(express.urlencoded({ extended: true }));
+/*
+mongoose.set("useFindAndModify", false);
+
+mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true }, () => {
+	console.log("Connected to db!");
+	app.listen(port, function() {
+  		console.log('Server Up and running!')
+	})
+}); */
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+	TodoTask.find({}, (err, tasks) => {
+		//res.render("index.ejs");
+		res.render("index.html"); //{ todoTasks: tasks });
+	});
+});
+
+
+app.get("/signup", (req, res) => {
+	TodoTask.find({}, (err, tasks) => {
+		res.render("signup.html");
+	});
+});
+
+
+app.get("/todo", (req, res) => {
+	TodoTask.find({}, (err, tasks) => {
+		res.render("todo.html", { todoTasks: tasks });
+	});
+});
+
+
+
+app.post('/',async (req, res) => {
+	const todoTask = new TodoTask({
+		content: req.body.content
+	});
+	try {
+		await todoTask.save();
+		res.redirect("/todo");
+	} catch (err) {
+		res.redirect("/todo");
+	}
+});
+
+app
+	.route("/edit/:id")
+	.get((req, res) => {
+	const id = req.params.id;
+	TodoTask.find({}, (err, tasks) => {
+		res.render("toDoUpdate.html", { todoTasks: tasks, idTask: id });
+		});
+	})
+	.post((req, res) => {
+		const id = req.params.id;
+		TodoTask.findByIdAndUpdate(id, { content: req.body.content }, err => {
+			if (err) return res.send(500, err);
+			res.redirect("/todo");
+		});
+	});
+
+
+app.route("/remove/:id").get((req, res) => {
+	const id = req.params.id;
+	TodoTask.findByIdAndRemove(id, err => {
+		if (err) return res.send(500, err);
+		res.redirect("/todo");
+	});
+});
+
+
+app.get('*', function(req, res) {
+  res.send({
+    error: 'This route does not exist, try /signup or /signin'
+  })
 })
